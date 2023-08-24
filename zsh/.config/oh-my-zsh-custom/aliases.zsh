7971bd1f698c9cea76bfd88d6f556f1bf8554a28
Patrick Zempel <patrick.zempel@mailbox.org>
2023-08-24 18:47:39 +0000

    Addes alias nvim->lvim

--- zsh/.config/oh-my-zsh-custom/aliases.zsh
+++ zsh/.config/oh-my-zsh-custom/aliases.zsh
@@ -7,6 +7,7 @@ alias c="clear"                         # clear the terminal
 alias rc="omz reload"                   # reload the zsh config
 ### Editor
 alias vim="lvim"                        # use lvim instead of vim
+alias nvim="lvim"                        # use lvim instead of nvim
 ### Pass
 alias pc="pass clip"                    # open fzf to search for passwords
 ################################################################################
