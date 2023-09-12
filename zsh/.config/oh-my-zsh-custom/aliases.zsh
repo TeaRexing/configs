710dd873e81af57289a1169a948617fa51a03dbf
Patrick Zempel <patrick.zempel@mailbox.org>
2023-09-12 17:20:18 +0200

    Added alias to kill all unnamed sessions

--- zsh/.config/oh-my-zsh-custom/aliases.zsh
+++ zsh/.config/oh-my-zsh-custom/aliases.zsh
@@ -3,6 +3,8 @@
 alias update="trizen -Syu"
 ### Terminal
 alias c="clear"                         # clear the terminal
+### TMUX
+alias tku="tmux ls -F'#{session_name}' | egrep '^[0-9]+$' |xargs -I% tmux kill-session -t \"=%\""
 ### ZSH
 alias rc="omz reload"                   # reload the zsh config
 ### Editor
