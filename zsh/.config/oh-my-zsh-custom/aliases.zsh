4c98c113a114d03f030238915bbf18ac91dec67c
Patrick Zempel <patrick.zempel@mailbox.org>
2023-10-03 14:31:36 +0200

    Added alias to open zsh configuration

--- zsh/.config/oh-my-zsh-custom/aliases.zsh
+++ zsh/.config/oh-my-zsh-custom/aliases.zsh
@@ -7,9 +7,10 @@ alias c="clear"                         # clear the terminal
 alias tku="tmux ls -F'#{session_name}' | egrep '^[0-9]+$' |xargs -I% tmux kill-session -t \"=%\""
 ### ZSH
 alias rc="omz reload"                   # reload the zsh config
+alias ozc="$EDITOR $HOME/.zshrc"        # open the zsh config
 ### Editor
 alias vim="lvim"                        # use lvim instead of vim
-alias nvim="lvim"                        # use lvim instead of nvim
+alias nvim="lvim"                       # use lvim instead of nvim
 ### Pass
 alias pc="pass clip"                    # open fzf to search for passwords
 ################################################################################
